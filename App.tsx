0036a81a86a994b176aeaa83ff23b8a7852a29e2
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2025-04-15 23:57:16 -0300

    feat: gluestack theme

--- App.tsx
+++ App.tsx
@@ -1,31 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import {
   Karla_400Regular,
   Karla_700Bold,
   useFonts,
 } from "@expo-google-fonts/karla";
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from "./config/gluestack-ui.config";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
 
   return (
-    <View style={styles.container}>
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: "Karla_700Bold" }}>Open up App.tsx to start working on your app!</Text>
-      ) : (
-        <View />
-      )}
-      <StatusBar style="dark" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <Center flex={1}>
+        {fontsLoaded ? (
+          <Text color="$blueLight">Open up App.tsx to start working on your app!</Text>
+        ) : (
+          <View />
+        )}
+        <StatusBar style="dark" />
+      </Center>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
