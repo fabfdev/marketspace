7bf3065eb324c7e4c7da5ae1e308eedec4339c1e
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2025-04-15 23:22:35 -0300

    feat: initial config

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Karla_400Regular,
+  Karla_700Bold,
+  useFonts,
+} from "@expo-google-fonts/karla";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: "Karla_700Bold" }}>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View />
+      )}
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
